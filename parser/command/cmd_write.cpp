f4c880d3add19bcef9ab8c8d60d85043b8bedd3b
Penguin <chenguanyou9338@gmail.com>
2024-06-07 01:25:11 +0800

    format command wirte option
    Signed-off-by: Penguin <chenguanyou9338@gmail.com>

--- parser/command/cmd_write.cpp
+++ parser/command/cmd_write.cpp
@@ -18,17 +18,43 @@
 #include "command/cmd_write.h"
 #include "base/utils.h"
 #include "api/core.h"
+#include <unistd.h>
+#include <getopt.h>
 
 int WriteCommand::main(int argc, char* const argv[]) {
-    if (!CoreApi::IsReady() || (argc < 3))
+    if (!CoreApi::IsReady() || (argc < 3)) {
+        usage();
         return 0;
+    }
 
     uint64_t address = Utils::atol(argv[1]) & CoreApi::GetVabitsMask();
-    uint64_t value = Utils::atol(argv[2]);
-    CoreApi::Write(address, value);
+    int opt;
+    int option_index = 0;
+    optind = 0; // reset
+    static struct option long_options[] = {
+        {"string",  required_argument, 0, 's'},
+        {"value",   required_argument, 0, 'v'},
+        {0,         0,                 0,  0 }
+    };
+
+    while ((opt = getopt_long(argc, argv, "s:v:",
+                long_options, &option_index)) != -1) {
+        switch (opt) {
+            case 's':
+                CoreApi::Write(address, optarg, strlen(optarg) + 1);
+                break;
+            case 'v':
+                uint64_t value = Utils::atol(optarg);
+                CoreApi::Write(address, value);
+                break;
+        }
+    }
     return 0;
 }
 
 void WriteCommand::usage() {
-    LOGI("Usage: write|wd <Address> <Value>\n");
+    LOGI("Usage: write|wd <Address> <Option>\n");
+    LOGI("Option:\n");
+    LOGI("    --string|-s <STRING>:\n");
+    LOGI("    --value|-v <VALUE>:\n");
 }
