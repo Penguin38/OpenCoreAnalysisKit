6e17ef20f1d1b1a67ca121d609e90c4980af8f39
Penguin <chenguanyou9338@gmail.com>
2024-12-13 00:07:07 +0800

    replace ANDROID_NDK to ANDROID_NDK_HOME
    Signed-off-by: Penguin <chenguanyou9338@gmail.com>

--- capstone.sh
+++ capstone.sh
@@ -24,18 +24,18 @@ cmake -DCMAKE_C_COMPILER=$BUILD_HOST_C_COMPILER \
 make -C $INSTALL_OUTPUT/linux/capstone -j8
 
 if [ $BUILD_PRODUCT == "aosp" ];then
-if [ -z $ANDROID_NDK ];then
-    echo "ANDROID_NDK is not set"
+if [ -z $ANDROID_NDK_HOME ];then
+    echo "ANDROID_NDK_HOME is not set"
     echo "Example:"
-    echo "    export ANDROID_NDK=NDK_DIR"
+    echo "    export ANDROID_NDK_HOME=NDK_DIR"
     echo "    ./build.sh"
     exit
 fi
 for CURRENT_ANDROID_ABI in $BUILD_ANDROID_ABIS
 do
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
       -DANDROID_ABI=$CURRENT_ANDROID_ABI \
-      -DANDROID_NDK=$ANDROID_NDK \
+      -DANDROID_NDK=$ANDROID_NDK_HOME \
       -DANDROID_PLATFORM=android-30 \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
