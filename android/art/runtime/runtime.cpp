58122f8a91bdd7ff601c46f600222ab160e2c294
Penguin. <chenguanyou9338@gmail.com>
2025-05-09 01:38:37 +0800

    fix macros __ART_RUNTIME_MAIN_PROP_PARSER__
    Signed-off-by: Penguin. <chenguanyou9338@gmail.com>

--- android/art/runtime/runtime.cpp
+++ android/art/runtime/runtime.cpp
@@ -445,20 +445,19 @@ Runtime& Runtime::Current() {
 
 #if defined(__ART_RUNTIME_MAIN_PROP_PARSER__)
         if (Android::Sdk() >= Android::V && runtime.Ptr()) {
-            uint32_t offset = OFFSET(Runtime, heap_);
             uint64_t point_size = CoreApi::GetPointSize();
             int32_t counter = -1;
             LoadBlock* block = nullptr;
             do {
                 counter++;
-                offset += (counter * point_size);
+                uint32_t offset = OFFSET(Runtime, heap_) + (counter * point_size);
                 // maybe filter finalizer_timeout_ms_
                 if (runtime.valueOf(offset) & 0xFFFFFFFFFFF00000ULL)
                     block = CoreApi::FindLoadBlock(runtime.valueOf(offset) , false);
             } while (!block && counter < 100);
 
             if (counter && counter < 100 && block) {
-                uint32_t cloc_off = offset - OFFSET(Runtime, heap_);
+                uint32_t cloc_off = counter * point_size;
                 __Runtime_offset__.heap_ += cloc_off;
                 __Runtime_offset__.monitor_pool_ += cloc_off;
                 __Runtime_offset__.thread_list_ += cloc_off;
